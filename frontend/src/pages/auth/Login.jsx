488f5c008dc3c0d29a96078c9c3a3a30bba94208
Babita Bist <bistababita17@gmail.com>
2025-03-07 19:54:22 +0545

    commit

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
                 if (response.data.user.role === 'admin') {
                     navigate('/admin/dashboard'); // Redirect Admins
                 } else {
-                    navigate('/dashboard'); // Redirect Student
+                    navigate('/dashboard'); // Redirect Students
                 }
             }, 500);
         } catch (error) {
