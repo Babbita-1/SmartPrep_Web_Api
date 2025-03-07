29015bd1b7425dab371ba60ea22e346ca0f28797
Babita Bist <bistababita17@gmail.com>
2025-03-07 19:46:05 +0545

    done

--- frontend/src/pages/admin/AddResource.jsx
+++ frontend/src/pages/admin/AddResource.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../../services/api';
 
-//addresources
 const AddResource = () => {
   const [formData, setFormData] = useState({ subjectId: '', title: '', type: '', url: '' });
   const navigate = useNavigate();
